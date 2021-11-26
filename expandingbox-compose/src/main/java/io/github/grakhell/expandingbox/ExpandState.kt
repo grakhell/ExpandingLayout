7a884ca0b17312eb1c089a22817e5f5894fc18ef
grakhell <grakhell>
2021-11-26 22:13:26 +0600

    Initial commit
    Composable expanding box init

---
+++ expandingbox-compose/src/main/java/io/github/grakhell/expandingbox/ExpandState.kt
@@ -0,0 +1,13 @@
+package io.github.grakhell.expandingbox
+
+import androidx.annotation.IntDef
+
+@Retention(AnnotationRetention.SOURCE)
+@IntDef(EXPANDED, EXPANDING, COLLAPSING, COLLAPSED, FIXED_SIZE)
+annotation class ExpandState
+
+const val EXPANDED = 0
+const val EXPANDING = 1
+const val COLLAPSING = 2
+const val COLLAPSED = 3
+const val FIXED_SIZE = 4
