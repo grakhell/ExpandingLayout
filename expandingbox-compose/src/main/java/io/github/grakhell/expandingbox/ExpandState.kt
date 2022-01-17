460278240969ab73579208328c5edaef0e712b5f
grakhell <grakhell>
2022-01-17 16:57:44 +0600

    Auto stash before rebase of "master"

--- expandingbox-compose/src/main/java/io/github/grakhell/expandingbox/ExpandState.kt
+++ expandingbox-compose/src/main/java/io/github/grakhell/expandingbox/ExpandState.kt
@@ -1,13 +1,11 @@
 package io.github.grakhell.expandingbox
 
-import androidx.annotation.IntDef
+import androidx.annotation.FloatRange
 
-@Retention(AnnotationRetention.SOURCE)
-@IntDef(EXPANDED, EXPANDING, COLLAPSING, COLLAPSED, FIXED_SIZE)
-annotation class ExpandState
-
-const val EXPANDED = 0
-const val EXPANDING = 1
-const val COLLAPSING = 2
-const val COLLAPSED = 3
-const val FIXED_SIZE = 4
+sealed class ExpandState(@FloatRange(from = 0.0, to = 1.0) fraction:Float) {
+    object Expanded:ExpandState(1f)
+    class Expanding(@FloatRange(from = 0.01, to = 0.09)fr:Float):ExpandState(fr)
+    class Collapsing(@FloatRange(from = 0.01, to = 0.09)fr:Float):ExpandState(fr)
+    object Collapsed:ExpandState(0f)
+    class FixedSized(@FloatRange(from = 0.0, to = 1.0) fr:Float):ExpandState(fr)
+}
--- expandingbox-compose/src/main/java/io/github/grakhell/expandingbox/ExpandingBox.kt
+++ expandingbox-compose/src/main/java/io/github/grakhell/expandingbox/ExpandingBox.kt
@@ -1,2 +1,24 @@
 package io.github.grakhell.expandingbox
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+
+@Composable
+fun ExpandingBox(
+    state: ExpandState = ExpandState.Expanded,
+    content: @Composable () -> Unit) {
+    val _state by remember { mutableStateOf(state) }
+
+
+}
+
+@Composable
+private fun ExpandingBoxStateless(
+    content: @Composable () -> Unit) {
+    Box() {
+        content.invoke()
+    }
+}
